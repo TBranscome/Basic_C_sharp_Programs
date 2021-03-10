9d7ac2b82ab0b5a7fcde38ba57a4c0464c83db7a
TBranscome <tbranscome11@gmail.com>
2021-03-10 18:50:34 -0500

    add boolean project
    added boolean  for car insurance approval project

---
+++ CarInsuranceProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace CarInsuranceProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Car Insurance Approval");
+            Console.WriteLine();//line break
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());//casting user input to int
+            Console.WriteLine("Have you ever had a DUI?");
+            Console.WriteLine("Please answer true or false.");//asking to provide either true or false input
+            string DUI = Console.ReadLine();//assigning user input to string variable
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());//casting user input to int variable
+            bool approved = age > 15 && DUI == "false" && tickets <= 3;//using boolean to answer true if age is > 15, DUI equals false, and tickets < or equal to 3
+            Console.WriteLine();//line break
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(approved);//using bool variable to print true or false depending if conditions met in bool variable "approved"
+            Console.ReadLine();
+        }
+    }
+}
