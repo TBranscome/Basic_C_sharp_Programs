cb6fc7b9ac6d2f0855d6c9f34831a99a26842fb5
TBranscome <tbranscome11@gmail.com>
2021-03-11 23:55:25 -0500

    add while loop
    added while loop to project

--- While_DoWhile/Program.cs
+++ While_DoWhile/Program.cs
@@ -8,42 +8,50 @@ namespace While_DoWhile
         static void Main()
         {
             Console.WriteLine("Can you guess my favorite color?");
-            Console.WriteLine("Guess a color:");
-            string color = Console.ReadLine();//user input string value
-            bool favColor = color == "purple";//variable favColor is true when purple is input
+            bool favColor = false;
 
-            do//do while loop added so the correct guess is still displayed properly
+            do //do while loop added so the correct guess is still displayed properly
             {
+                Console.WriteLine("Guess a color:");
+                string color = Console.ReadLine();
+
                 switch (color)//switch statement using the input color
                 {
-                    case "blue"://case for the input 
-                        Console.WriteLine("You guessed blue. Try again.");
-                        Console.WriteLine("Guess a color:");
-                        color = Console.ReadLine();//user input
-                        break;//break so loop ends without running the ret of cases automatically
-                    case "red":
-                        Console.WriteLine("You guessed red. Try again.");
-                        Console.WriteLine("Guess a color:");
-                        color = Console.ReadLine();
-                        break;
-                    case "yellow":
-                        Console.WriteLine("You guessed yellow. Try again.");
-                        Console.WriteLine("Guess a color:");
-                        color = Console.ReadLine();
-                        break;
+                    
                     case "purple":
                         Console.WriteLine("You guessed purple. That is correct!!!");
                         favColor = true;//stating favColor is true and the answer is correct
                         break;
                     default://defaut for any answer not associated with a case
                         Console.WriteLine("You guessed " + color + ". Try again.");
-                        Console.WriteLine("Guess a color:");
-                        color = Console.ReadLine();
                         break;
                 }
             }
-            while (!favColor);//while loop to be used when favColor is false
+            while (!favColor);//loop to be performed when favColor is true
+            {
 
+                Console.WriteLine("What is my favorite number 1 through 5?");
+                bool favNum = false;
+                
+                while (!favNum)//while loop for favorite number
+                {
+
+                    Console.WriteLine("Guess a number?");
+                    int number = Convert.ToInt32(Console.ReadLine());
+
+                    switch (number)
+                    {
+                        case 5:
+                            Console.WriteLine("That is correct!");
+                            favNum = true;
+                            break;
+                        default:
+                            Console.WriteLine("Your guess of: " + number + " is not correct.");
+                            break;
+                    }
+                }
+            }
+            
             Console.Read();
         }
     }
