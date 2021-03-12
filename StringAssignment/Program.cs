bb1b6726ef69345e7553d7f9e5a8a4953e5e5c9c
TBranscome <tbranscome11@gmail.com>
2021-03-12 17:59:58 -0500

    Create new project to show string functions and stringbuilder

---
+++ StringAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            //assigning string variables
+            string name = "Tyler";
+            string quote = " is concatenating ";
+            string endQuote = "three strings!";
+
+            //converts string to uppercase
+            name = name.ToUpper();
+
+            //concatenating the 3 strings
+            Console.WriteLine(name + quote + endQuote);
+
+            //Create a stringbuilder for paragraph with new line \n
+            StringBuilder sb = new StringBuilder("This is StringBuilder.\n");
+
+            //append a new sentence to end of first line but on new line \n
+            sb.Append("Adding one sentence to the paragraph.\n");
+
+            //append sb with new line sentence
+            sb.Append("One line at a time.");
+
+            Console.WriteLine(sb);//display stringbuilder and its strings
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ StringFunctions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Tyler";
+            //string quote = "The man said, \"Hello, Tyler.\"\nHello on a new line.\n\tHello on a new tab.";
+            //string fileName = @"C:\Users\tbran";
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("r");
+
+            //int length = name.Length;
+
+            //name = name.ToUpper();
+
+            //Console.WriteLine(length);
+            //Console.WriteLine(quote);
+            //Console.WriteLine(trueOrFalse);
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+           
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is Tyler");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
