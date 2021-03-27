71312975810fc1c085eac7c406790fe6e8c5014c
TBranscome <tbranscome11@gmail.com>
2021-03-27 00:37:12 -0400

    Call method and class

---
+++ CallingMethodsSubmission/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmission
+{
+    public class MyMathClass
+    {
+        public int Area()
+        {
+            Console.WriteLine("Input a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input a second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int b = num1;
+            int h = num2;
+            Console.WriteLine($"If your numbers were the base and height of a square, the area would be:");
+            return b * h;
+
+        }
+        public static int OnePlus()
+        {
+            Console.WriteLine("Input a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine($"Your numbers plus one are:");
+            return num1+1;
+        }
+        public static int MinusOne()
+        {
+            Console.WriteLine("Input a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine($"Your numbers minus one are:");
+            return num1-1;
+            
+        }
+    }
+}
---
+++ CallingMethodsSubmission/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMathClass MathObject = new MyMathClass();
+            Console.WriteLine(MathObject.Area());
+            Console.WriteLine(MyMathClass.OnePlus());
+            Console.WriteLine(MyMathClass.MinusOne());
+            Console.ReadLine();
+            
+
+        }
+    }
+}
