0a4cf25e7436d56b77a10b74f15937828150a117
TBranscome <tbranscome11@gmail.com>
2021-03-28 18:00:02 -0400

    create project
    Created project to show methods and overloads using math operations on different data types

---
+++ MainMethodSubmission/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    class MyMathClass
+    {
+        public int MainMath(int num1)
+        {
+            int num2 = 2;
+            Console.WriteLine("Your number multiplied by two is:");
+            return num1 * num2;
+        }
+        public double MainMath(double numPlus)
+        {
+            double num2 = 5.45;
+            Console.WriteLine("Your number plus 5.45 as an integer is:");
+            int num1ToInt = Convert.ToInt32(numPlus);
+            int num2ToInt = Convert.ToInt32(num2);
+            return num1ToInt + num2ToInt;
+        }
+        public int MainMath(string numMinus)
+        {
+            int numConvert = Convert.ToInt32(numMinus);
+            int num2 = 77;
+            Console.WriteLine("Your number subtracted from seventy seven is:");
+            return num2 - numConvert;
+        }
+    }
+}
---
+++ MainMethodSubmission/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMathClass mathObject1 = new MyMathClass();
+            Console.WriteLine("Input a whole number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(mathObject1.MainMath(num1));
+            //
+            MyMathClass mathObject2 = new MyMathClass();
+            Console.WriteLine("input a decimal number:");
+            double numPlus = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(mathObject2.MainMath(numPlus));
+            //
+            MyMathClass mathObject3 = new MyMathClass();
+            Console.WriteLine("Input a whole number:");
+            string numMinus = Console.ReadLine();
+            Console.WriteLine(mathObject3.MainMath(numMinus));
+            //
+            Console.ReadLine();
+        }
+    }
+}
