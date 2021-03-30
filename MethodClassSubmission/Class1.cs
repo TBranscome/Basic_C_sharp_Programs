4c7dd90df8bfba5d15db8ffbbecf838028a6db42
TBranscome <tbranscome11@gmail.com>
2021-03-29 21:33:08 -0400

    create project
    created method project using void method

---
+++ MethodClassSubmission/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmission
+{
+    class MathClass
+    {
+        public static void MathOp(int num1, int num2)
+        {
+            int total = num1 + 25;
+            Console.WriteLine($"Your first number plus 25 is: {total}");
+            Console.WriteLine($"Now you add: {num2}");
+            
+        }
+    }
+}
---
+++ MethodClassSubmission/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathClass mathClass = new MathClass();
+            int num1 = 78;
+            int num2 = 30;
+
+            MathClass.MathOp(num1, num2); 
+
+            Console.WriteLine();
+
+            MathClass.MathOp(87, 90);
+
+            Console.ReadLine();
+        }
+    }
+}
