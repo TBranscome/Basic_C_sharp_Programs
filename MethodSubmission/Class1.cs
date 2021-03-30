aa2fa3bca2951f1753750f7b0a91940b0fc0f964
TBranscome <tbranscome11@gmail.com>
2021-03-29 20:53:25 -0400

    Create method project
    created project using method optional parameters

---
+++ MethodSubmission/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    class MathOperation
+    {
+        public int MathOp(int num1, int num2 = 0)
+        {
+            int total = num1 + num2 + 7;
+            Console.WriteLine("Your number/numbers added together then added to seven are:");
+            return total;
+        }
+    }
+}
---
+++ MethodSubmission/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation mathObject = new MathOperation();
+            Console.WriteLine("Input a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("If you want, please enter a second number, though this is not necessary.");
+            int num2;
+            try
+            {
+                num2 = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                num2 = 0;
+            }
+            Console.WriteLine(mathObject.MathOp(num1, num2));
+            Console.ReadLine();
+
+
+
+
+            
+            
+        }
+    }
+}
