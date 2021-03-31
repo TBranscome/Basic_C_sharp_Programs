828fbbb3535615a9c14914ec4ffd8b533293d682
TBranscome <tbranscome11@gmail.com>
2021-03-31 14:12:35 -0400

    add comments

--- AbstractClassSubmission/Program.cs
+++ AbstractClassSubmission/Program.cs
@@ -11,7 +11,7 @@ namespace AbstractClassSubmission
         static void Main(string[] args)
         {
             //5.Implement the SayName() method inside of the Employee class.
-            Employee employee = new Employee() { firstName = "Sample", lastName = "Student" };
+            Employee employee = new Employee() { firstName = "Sample", lastName = "Student" }; //instantiate an object and give values to its attributes
             employee.SayName();
             //Use polymorphism to create an object of type IQuittable and call the Quit() method on it
             IQuittable quittable = new Employee();
