80a1482829b6265bed35b1a9104937ab17099733
TBranscome <tbranscome11@gmail.com>
2021-03-31 18:14:52 -0400

    create project
    created project that overloads the == and != operator

---
+++ OperatorsSubmission/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission
+{
+    public class Employee : Person
+    {
+        
+        
+        public int Id { get; set; }
+        // Overloading the == operator to compare Id of employees
+        public static bool operator == (Employee employee1, Employee employee2)
+        {
+            if (employee1.Id == employee2.Id)
+            {
+                Console.WriteLine("The ID's are equal");
+                return true;//returns response if equal
+            }
+            else
+            {
+                Console.WriteLine("The ID's are not equal");
+                return false;
+            }
+        }
+        // Overloads != operator for comparing employee Id. Has to be included for == to work
+        public static bool operator != (Employee employee1, Employee employee2)
+        {
+            if (employee1.Id != employee2.Id)
+            {
+                Console.WriteLine("The ID's are not equal");
+                return true;//returns response if not equal
+            }
+            else
+            {
+                Console.WriteLine("The ID's are equal");
+                return false;
+            }
+        }
+        
+
+        
+    }
+}
---
+++ OperatorsSubmission/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate employee1 with Id of 1
+            Employee employee1 = new Employee() { firstName = "Tyler", lastName = "B", Id = 1 };
+            //Instantiate employee2 with Id of 2
+            Employee employee2 = new Employee() { firstName = "Emily", lastName = "T", Id = 2 };
+            //Instantiate employee3 with Id of 1
+            Employee employee3 = new Employee() { firstName = "Grayson", lastName = "B", Id = 1 };
+            //Compare Id of employee1 with employee2 and print response
+            bool doesEqual = employee1 == employee2;
+            //Compare Id of employee1 with employee3 and print response
+            doesEqual = employee1 == employee3;
+            Console.ReadLine();
+
+            
+            
+
+
+        }
+    }
+}
