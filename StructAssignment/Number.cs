7b05bab738fe33abb8d40e27de7d4c2c918a5a4c
TBranscome <tbranscome11@gmail.com>
2021-04-01 17:07:53 -0400

    create project
    created project to show struct value type

---
+++ StructAssignment/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    public struct Number
+    {
+        //setting property to double for decimal amounts
+        public double Amount { get; set; }
+    }
+}
---
+++ StructAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating new number struct and giving decimal value
+            Number number1 = new Number();
+            number1.Amount = 1.95;
+            //printing amount to console
+            Console.WriteLine(number1.Amount);
+            Console.ReadLine();
+        }
+    }
+}
