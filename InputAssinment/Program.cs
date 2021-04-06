fa0b33ed7e6b3a15527d2ccd8038d3105e3590aa
TBranscome <tbranscome11@gmail.com>
2021-04-06 15:02:57 -0400

    update file
    updated file to include try catch block for incorrect data type input

--- InputAssinment/Program.cs
+++ InputAssinment/Program.cs
@@ -13,31 +13,35 @@ namespace InputAssinment
         {
             //asking user to input a number
             Console.WriteLine("Input a number");
-            
             //bool flag for while loop 
             bool continueInput = true;
             //loop for more inputs if desired
             while (continueInput == true)
             {
-                //casting number to int
-                int userInput = Convert.ToInt32(Console.ReadLine());
-                //using streamwriter to log input to text file, and letting it append new inputs
-                using (StreamWriter file = new StreamWriter(@"C:\Users\tbran\Logs\assignLog.txt", true))
+                try //try catch block for user input data type. Must be able to cast to int
+                { 
+                    //asking for user input
+                    int userInput = Convert.ToInt32(Console.ReadLine());
+                    //using streamwriter to log input to text file, and letting it append new inputs
+                    using (StreamWriter file = new StreamWriter(@"C:\Users\tbran\Logs\assignLog.txt", true))
                     {
                         file.WriteLine(userInput);
                     }
-                Console.WriteLine($"The input: {userInput}, has been logged");
-                Console.WriteLine("More inputs? Please answer yes or no.");
-                string answer = Console.ReadLine().ToLower();
-                if (answer == "no")
-                {
-                    //ends while loop condition
-                    continueInput = false;
+                    Console.WriteLine($"The input: {userInput}, has been logged");
+                    Console.WriteLine("More inputs? Please answer yes or no.");
+                    string answer = Console.ReadLine().ToLower();
+                    if (answer == "no")
+                    {
+                        //ends while loop condition
+                        continueInput = false;
+                    }
                 }
-                
+                catch (FormatException)//format exception for data type
+                {
+                    continueInput = true;//continues while loop
+                    Console.WriteLine("Please input a whole number");
+                }
             }
-            
-            
             Console.WriteLine("Input Log:");
             //using streamreader to print all inputs to screen, inputs from previous console runs too.
             using (StreamReader file = new StreamReader(@"C:\Users\tbran\Logs\assignLog.txt"))
