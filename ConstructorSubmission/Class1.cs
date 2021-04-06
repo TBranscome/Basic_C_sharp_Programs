af288ee4e93b5c08622ed3741ba15a1f02233767
TBranscome <tbranscome11@gmail.com>
2021-04-06 17:57:02 -0400

    create project
    created project for chained constructors and using the var keyword, along with constants

---
+++ ConstructorSubmission/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmission
+{
+    public class Person
+    {
+        //chaining two constructors together
+        public Person(string fname) : this(fname, "Branscome")// gives last name when only first name is defined
+        {
+        }
+        public Person(string fname, string lname)//constructor taking two strings, first and last names
+        {
+            FirstName = fname;
+            LastName = lname;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ ConstructorSubmission/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create constant variable
+            const string myName = "My name is:";
+            //using var keyword, created variable from Person
+            var newPerson1 = new Person("Tyler");
+            Person newPerson2 = new Person("Emily", "Thunberg");
+            //print const and var
+            Console.WriteLine($"{myName} {newPerson1.FirstName} {newPerson1.LastName}");
+            Console.WriteLine($"{myName} {newPerson2.FirstName} {newPerson2.LastName}");
+            Console.ReadLine();
+        }
+    }
+}
